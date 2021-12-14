a0e240a49396d22e81dd0f90a6697ed9adae4a9b
mohsen karimi <themohsenkarimi@gmail.com>
2021-12-14 14:38:29 +0430

    first commit

---
+++ app/Http/Controllers/ChartController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ChartController extends Controller
+{
+    //
+    public function index(){
+
+        $userData = User::select(DB::raw("COUNT(*) as count"))
+                    ->whereYear('created_at', date('Y'))
+                    ->groupBy(DB::raw("Month(created_at)"))
+                    ->pluck('count');
+
+
+        return view('/home', compact('userData'));
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <title>Laravel 8 Highcharts Demo</title>
+</head>
+
+<body>
+    <center><h1 style="color:red;">Highcharts in Laravel 8 Example</h1></center>
+   
+	<div id="container"></div>
+</body>
+
+<script src="https://code.highcharts.com/highcharts.js"></script>
+
+<script type="text/javascript">
+    var userData = <?php echo json_encode($userData)?>;
+
+    Highcharts.chart('container', {
+        title: {
+            text: 'New User 2021'
+        },
+        subtitle: {
+            text: 'Bluebird youtube channel'
+        },
+        xAxis: {
+            categories: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September',
+                'October', 'November', 'December'
+            ]
+        },
+        yAxis: {
+            title: {
+                text: 'Number of New Users'
+            }
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle'
+        },
+        plotOptions: {
+            series: {
+                allowPointSelect: true
+            }
+        },
+        series: [{
+            name: 'New Users',
+            data: userData
+        }],
+        responsive: {
+            rules: [{
+                condition: {
+                    maxWidth: 500
+                },
+                chartOptions: {
+                    legend: {
+                        layout: 'horizontal',
+                        align: 'center',
+                        verticalAlign: 'bottom'
+                    }
+                }
+            }]
+        }
+    });
+
+</script>
+
+</html>
